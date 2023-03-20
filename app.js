9cd8c1591a8793cc6f0d7348f27b39850c59d750
prajilavinod <107175014+prajilavinod16@users.noreply.github.com>
2023-03-20 21:32:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'jd_assignment' 
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
+//Server
+app.listen(3000, ()=>{
+
+    console.log('server is running http://localhost:3000');
+})
